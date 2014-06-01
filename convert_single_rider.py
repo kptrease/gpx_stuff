d9ca21b30a84dd50ca1bc7eb1784cc2b87a257c0
Kenneth Trease <kptrease@gmail.com>
2014-06-01 01:57:29 -0700

    Update to make user input work
    Using OptionParser, which I read, after implementation, is now
    deprecated? Need to fix up to argparse, I guess.
    
    Would like to guarantee that the output file defaults to the path your
    script is in, but right now, it's defaulting to the python path for me.

--- convert_single_rider.py
+++ convert_single_rider.py
@@ -2,20 +2,36 @@ import codecs
 import glob
 import os
 import xml.etree.ElementTree as ET
+import sys
+from optparse import OptionParser
 
-# TODO: override this with a value input from the user
-output_file = 'traces.csv'
-# change this if you want the output file to use a different separator (if your user used commas in their data, for example
-# separator = "|"
-separator = ","
+parser = OptionParser()
+parser.add_option('-i', '--input', dest='data_dir', type='string', action='store',
+                  help='path to the directory your gpx files live in')
+parser.add_option('-o', '--output', dest='output_file', type='string', action='store', default='traces.csv',
+                  help='what you\'d like your output file to be called. Default is traces.csv. Include full destination '
+                       'file path to put it where you want.')
+parser.add_option('-d', '--delimiter', dest='delimiter', type='string', action='store', default=',',
+                  help='what delimiter would you like to use? Default is comma, but use another if your user uses commas'
+                       'in their trace names')
+parser.set_defaults(verbose=True)
+(options, args) = parser.parse_args()
+
+if options.data_dir:
+    data_dir = options.data_dir
+if options.output_file:
+    output_file = options.output_file
+if options.delimiter:
+    delimiter = options.delimiter
 
 # open or create the output file, and add header information on the first line
 with codecs.open(output_file, "w+", encoding='utf8') as myfile:
-    myfile.write('ride' + separator + 'latitude ' + separator + 'longitude' + separator + 'elevation' + separator + 'time\n')
+    myfile.write('ride' + delimiter + 'latitude' + delimiter + 'longitude' + delimiter + 'elevation' + delimiter + 'time\n')
 
 traces = []
-# TODO: get this path from input. Right now, hard-coded to my local machine's dir structure
-for file in glob.glob("C:\Python33\working\data\gpx_traces\*.gpx"):
+
+data_dir = data_dir + "\*.gpx"
+for file in glob.glob(data_dir):
     traces.append(file)
 
 for trace in traces:
@@ -43,6 +59,6 @@ for trace in traces:
                             if 'time' in data.tag:
                                 time = data.text
 
-                        line = line + name + separator + lat_long['lat'] + separator + lat_long['lon'] + separator + ele + separator + time + "\n"
+                        line = line + name + delimiter + lat_long['lat'] + delimiter + lat_long['lon'] + delimiter + ele + delimiter + time + "\n"
         with codecs.open(output_file, "a", encoding='utf8') as myfile:
             myfile.write(line)
