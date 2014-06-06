a92114b1ab58d9e8b917d746646e750e1700d5dc
Kenneth Trease <kptrease@gmail.com>
2014-06-06 07:13:49 -0700

    Update with a privacy feature
    Allows the first and last n points in a trace to be stripped out before
    writing to an output file.

--- convert_single_rider.py
+++ convert_single_rider.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 import sys
 from optparse import OptionParser
 
+#TODO: OptionParser is deprecated, so need to switch over to ArgParse
 parser = OptionParser()
 parser.add_option('-i', '--input', dest='data_dir', type='string', action='store',
                   help='path to the directory your gpx files live in')
@@ -14,6 +15,7 @@ parser.add_option('-o', '--output', dest='output_file', type='string', action='s
 parser.add_option('-d', '--delimiter', dest='delimiter', type='string', action='store', default=',',
                   help='what delimiter would you like to use? Default is comma, but use another if your user uses commas'
                        'in their trace names')
+
 parser.set_defaults(verbose=True)
 (options, args) = parser.parse_args()
 
@@ -23,6 +25,11 @@ if options.output_file:
     output_file = options.output_file
 if options.delimiter:
     delimiter = options.delimiter
+
+# set this value to obscure the endpoints (protect the privacy of the people provding the traces
+# strips out this many points from the beginning and end of each ride
+# TODO: would like to make this a user parameter, but having trouble thinking throug the semantics of a boolean param. Could just make it an int
+points_to_obscure = 50
 
 # open or create the output file, and add header information on the first line
 with codecs.open(output_file, "w+", encoding='utf8') as myfile:
@@ -41,14 +48,14 @@ for trace in traces:
 
     # there is a metadata child and a trk child. Everything we do is on the trk child. Could make this more explicit.
     for data in ride:
-        line = ""
 
-        for trkseg in data:
-            if('name' in trkseg.tag):
-                name = trkseg.text
+        for trk in data:
+            if('name' in trk.tag):
+                name = trk.text
 
-            if('trkseg' in trkseg.tag):
-                for trkpt in trkseg:
+            if('trkseg' in trk.tag):
+                lines = []
+                for trkpt in trk:
                     if('trkpt' in trkpt.tag):
                         # the attrib here is a dictionary with lat and long in it.
                         lat_long = trkpt.attrib
@@ -58,7 +65,9 @@ for trace in traces:
                                 ele = data.text
                             if 'time' in data.tag:
                                 time = data.text
+                        line = name + delimiter + lat_long['lat'] + delimiter + lat_long['lon'] + delimiter + ele + delimiter + time + "\n"
+                        lines.append(line)
 
-                        line = line + name + delimiter + lat_long['lat'] + delimiter + lat_long['lon'] + delimiter + ele + delimiter + time + "\n"
-        with codecs.open(output_file, "a", encoding='utf8') as myfile:
-            myfile.write(line)
+                with codecs.open(output_file, "a", encoding='utf8') as myfile:
+                    for i in range(points_to_obscure, (len(lines) - points_to_obscure)):
+                        myfile.write(lines[i])
